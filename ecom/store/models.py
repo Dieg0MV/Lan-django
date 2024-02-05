788befd2007ceaf7e2e2d9538a6dea404d0cc9fd
Dieg0MV <diegoma.v2004@gmail.com>
2024-02-04 22:55:39 -0600

    version 0.2 update store

--- ecom/store/models.py
+++ ecom/store/models.py
@@ -6,5 +6,8 @@ class Product(models.Model):
     productname = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
-    image = models.ImageField()
+    image = models.TextField()
+    
+    def __str__(self):
+        return self.productname
     
--- ecom/store/views.py
+++ ecom/store/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
+from django.http import JsonResponse
 from .models import *
 from django.shortcuts import get_object_or_404
 # Create your views here.
+
+'Por ahora solo hace una peticion a esos datos devolviendolos en crudo'
 def hello(request):
-    return HttpResponse('Hello world')
-#datos mostrados en formato json
+    #products = list(Product.objects.values())
+    items = Product.objects.all()
+    return render(request, 'index.html',{
+        'items':items
+    })
+
+'''#datos mostrados en formato json
 def products(request):
     products = list(Product.objects.values())
-    return JsonResponse(products, safe=False)
+   ''' 
